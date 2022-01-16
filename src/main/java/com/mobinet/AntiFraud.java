a556aaf561634727ca5563a0c6396e26169cefac
Marcelo Trajano <marcelo.trajano@outlook.com>
2022-01-16 14:29:55 -0300

    new files

---
+++ src/main/java/com/mobinet/AntiFraud.java
@@ -0,0 +1,13 @@
+package com.mobinet;
+
+/**
+ * Hello world!
+ *
+ */
+public class AntiFraud
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
