778e7692f3e8f806940207afa7458ebc0d9ffab0
Marcelo Trajano <marcelo.trajano@outlook.com>
2022-01-30 06:14:13 -0300

    ＼(≧▽≦)／

---
+++ client/src/main/java/com/mobinet/client/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.mobinet.client;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository  {
+}
---
+++ client/src/main/java/com/mobinet/client/ClientService.java
@@ -0,0 +1,7 @@
+package com.mobinet.client;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+}
