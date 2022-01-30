2eb188a0cdf4a37f99fd48a9ec9e39ebb3ead2c0
Marcelo Trajano <marcelo.trajano@outlook.com>
2022-01-29 23:40:42 -0300

    ＼(≧▽≦)／

---
+++ client/src/main/java/com/mobinet/client/Client.java
@@ -0,0 +1,17 @@
+package com.mobinet.client;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Client {
+
+    private Long Id;
+    private String branchNumber;
+    private String accountNumber;
+    private String clientName;
+
+
+
+}
---
+++ client/src/main/java/com/mobinet/client/ClientController.java
@@ -0,0 +1,16 @@
+package com.mobinet.client;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/clients")
+public class ClientController {
+
+
+
+
+}
